d74daa4f3817c6f8544c28b07b03e264fd9c0d62
Daniel Fain <d6niel@protonmail.com>
2020-05-20 23:24:13 -0400

    Load a boilerplate docker compose file and print contents for testing

--- hms.py
+++ hms.py
@@ -1,12 +1,14 @@
 import click
+import yaml
 
 @click.command()
-@click.option("--count", default=1, help="Number of greetings.")
-@click.option("--name", prompt="Your name", help="The person to greet.")
-def generate(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for _ in range(count):
-        click.echo(f"Hello, {name}!")
+# @click.option("--count", default=1, help="Number of greetings.")
+# @click.option("--name", prompt="Your name", help="The person to greet.")
+def generate():
+    """Generates a docker-compose file for an automated home media server"""
+    with open(r'docker-compose.yml') as file:
+        boilerplate = yaml.load(file, Loader=yaml.SafeLoader)
+        print(boilerplate['services'])
 
 if __name__ == "__main__":
     generate()
