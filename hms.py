65a28d5bc199b213e7c01c5c074a05e5db7209ed
Daniel Fain <d6niel@protonmail.com>
2020-06-04 14:39:23 -0400

    Add sonarr, radarr, and jackett option flows

--- hms.py
+++ hms.py
@@ -10,41 +10,77 @@ def generate(puid, pgid, tz):
     compose_file = { "version": "2", "services": {} }
 
     plex_service = create_service("linuxserver/plex", "plex", ["PUID=" + str(puid), "PGID=" + str(pgid), "TZ=" + tz, "VERSION=docker"], [], [32400])
-    jackett_service = create_service("linuxserver/jackett", "jackett", ["PUID=" + str(puid), "PGID=" + str(pgid), "TZ=" + tz], [], [9117])
-    sonarr_service = create_service("linuxserver/sonarr", "sonarr", ["PUID=" + str(puid), "PGID=" + str(pgid), "TZ=" + tz], [], [8989])
-    radarr_service = create_service("linuxserver/radarr", "radarr", ["PUID=" + str(puid), "PGID=" + str(pgid), "TZ=" + tz], [], [7878])
 
     if click.prompt("Transmission + VPN", default="y", type=str) == "y":
         print("----TRANSMISSION SETUP----")
         provider = click.prompt("VPN Provider", type=str)
-        port = click.prompt("Transmission port", default=9091, type=int)
+        port = click.prompt("Port", default=9091, type=int)
 
         transmission_service = create_service("haugene/transmission-openvpn", 
             "transmission",
             ["PUID=" + str(puid), "PGID=" + str(pgid), "CREATE_TUN_DEVICE=true", "OPENVPN_PROVIDER=" + provider],
             [],
             [port])
+            
+        compose_file["services"]["transmission"] = transmission_service
+
         print("--------------------------")
 
     if click.prompt("NZBGet", default="y", type=str) == "y":
-        print("----NZBGET SETUP----")
-        port = click.prompt("NZBGet port", default=6789, type=int)
+        print("-------NZBGET SETUP-------")
+        port = click.prompt("Port", default=6789, type=int)
 
         nzbget_service = create_service("linuxserver/nzbget", 
-        "nzbget", 
-        ["PUID=" + str(puid), "PGID=" + str(pgid), "TZ=" + tz], 
-        [],
-        [port])
+            "nzbget", 
+            ["PUID=" + str(puid), "PGID=" + str(pgid), "TZ=" + tz], 
+            [],
+            [port])
+
+        compose_file["services"]["nzbget"] = nzbget_service
+
         print("--------------------------")
 
-    compose_file["services"] = { 
-        plex_service.get("container_name"): plex_service,
-        jackett_service.get("container_name"): jackett_service,
-        sonarr_service.get("container_name"): sonarr_service,
-        radarr_service.get("container_name"): radarr_service,
-        transmission_service.get("container_name"): transmission_service,
-        nzbget_service.get("container_name"): nzbget_service
-    }
+    if click.prompt("Sonarr", default="y", type=str) == "y":
+        print("-------SONARR SETUP-------")
+        port = click.prompt("Port", default=8989, type=int)
+
+        sonarr_service = create_service("linuxserver/sonarr", 
+            "sonarr", 
+            ["PUID=" + str(puid), "PGID=" + str(pgid), "TZ=" + tz], 
+            [],
+            [port])
+        
+        compose_file["services"]["sonarr"] = sonarr_service
+
+        print("--------------------------")
+
+    if click.prompt("Radarr", default="y", type=str) == "y":
+        print("-------RADARR SETUP-------")
+        port = click.prompt("Port", default=7878, type=int)
+
+        radarr_service = create_service("linuxserver/radarr", 
+            "radarr", 
+            ["PUID=" + str(puid), "PGID=" + str(pgid), "TZ=" + tz], 
+            [],
+            [port])
+
+        compose_file["services"]["radarr"] = radarr_service
+
+        print("--------------------------")
+
+    if click.prompt("Jackett", default="y", type=str) == "y":
+        print("------JACKETT SETUP-------")
+        port = click.prompt("Port", default=9117, type=int)
+
+        jackett_service = create_service("linuxserver/jackett", 
+            "jackett", 
+            ["PUID=" + str(puid), "PGID=" + str(pgid), "TZ=" + tz], 
+            [],
+            [port])
+
+        compose_file["services"]["jackett"] = jackett_service
+
+        print("--------------------------")
 
     file = open("docker-compose.yml", "w")
     file.write(yaml.dump(compose_file))
