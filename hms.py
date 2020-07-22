2644d1b80af3b8551bc3aefe3e76331f0287861a
Daniel Fain <d6niel@protonmail.com>
2020-07-22 13:40:34 -0400

    Path existence validation & PUID, PGID, and TZ

--- hms.py
+++ hms.py
@@ -1,136 +1,34 @@
-import click
+from PyInquirer import prompt, print_json
 import yaml
+import os
 
-@click.command()
-@click.option("--puid", prompt="PUID", type=int)
-@click.option("--pgid", prompt="PGID", type=int)
-@click.option("--tz", prompt="Timezone", default="America/New_York", type=str)
-@click.option("--config-path", prompt="Absolute path to config folder", type=str)
-def generate(puid, pgid, tz, config_path):
-    """Generates a docker-compose file for an automated home media server"""
-    compose_file = { "version": "2", "services": {} }
-
-    num_volumes = click.prompt("Number of media volumes for Plex", default=1, type=int)
-
-    media_vols = []
-
-    for i in range(num_volumes):
-        volume_path = click.prompt("Absolute path of media volume " + str(i + 1), type=str)
-        media_vols.append(volume_path + ":/Media" + str(i + 1))
-
-    media_vols.append("{}/plex:/config".format(config_path))
-    media_vols.append(config_path + "/plex/transcode:/transcode")
-
-    plex_service = create_service("linuxserver/plex", 
-        "plex", 
-        ["PUID=" + str(puid), "PGID=" + str(pgid), "TZ=" + tz, "VERSION=docker"], 
-        media_vols, 
-        [32400])
-
-    compose_file["services"]["plex"] = plex_service
-
-    if click.prompt("Transmission + VPN", default="y", type=str) == "y":
-        print("----TRANSMISSION SETUP----")
-        port = click.prompt("Port", default=9091, type=int)
-        provider = click.prompt("VPN Provider", type=str)
-        config = click.prompt("VPN Location", type=str)
-        username = click.prompt("VPN Username", type=str)
-        password = click.prompt("VPN Password", type=str)
-        lan = click.prompt("LAN Subnet", default="192.168.1.0/24" , type=str)
-
-        transmission_service = create_service("haugene/transmission-openvpn", 
-            "transmission",
-            ["PUID=" + str(puid), "PGID=" + str(pgid), "CREATE_TUN_DEVICE=true", "OPENVPN_PROVIDER=" + provider, 
-            "OPENVPN_CONFIG=" + config, "OPENVPN_USERNAME=" + username, "OPENVPN_PASSWORD=" + password, "LOCAL_NETWORK=" + lan],
-            ["{}/transmission/downloads:/downloads".format(config_path)],
-            [port])
-
-        transmission_service["cap_add"] = ["NET_ADMIN"]
-            
-        compose_file["services"]["transmission"] = transmission_service
-
-        print("--------------------------")
-
-    if click.prompt("NZBGet", default="y", type=str) == "y":
-        print("-------NZBGET SETUP-------")
-        port = click.prompt("Port", default=6789, type=int)
-
-        nzbget_service = create_service("linuxserver/nzbget", 
-            "nzbget", 
-            ["PUID=" + str(puid), "PGID=" + str(pgid), "TZ=" + tz], 
-            ["{}/nzbget:/config".format(config_path), 
-            "{}/nzbget/downloads:/downloads".format(config_path)],
-            [port])
-
-        compose_file["services"]["nzbget"] = nzbget_service
-
-        print("--------------------------")
-
-    if click.prompt("Sonarr", default="y", type=str) == "y":
-        print("-------SONARR SETUP-------")
-        port = click.prompt("Port", default=8989, type=int)
-        tv_path = click.prompt("Absolute path to Plex TV directory", type=str)
-
-        sonarr_service = create_service("linuxserver/sonarr", 
-            "sonarr", 
-            ["PUID=" + str(puid), "PGID=" + str(pgid), "TZ=" + tz], 
-            ["{}/sonarr:/config".format(config_path), 
-            "{}/transmission/downloads:/torrents".format(config_path),
-            "{}/nzbget/downloads:/nzbs".format(config_path),
-            tv_path + ":/tv"],
-            [port])
-        
-        compose_file["services"]["sonarr"] = sonarr_service
-
-        print("--------------------------")
-
-    if click.prompt("Radarr", default="y", type=str) == "y":
-        print("-------RADARR SETUP-------")
-        port = click.prompt("Port", default=7878, type=int)
-        movie_path = click.prompt("Absolute path to Plex movie directory", type=str)
-
-        radarr_service = create_service("linuxserver/radarr", 
-            "radarr", 
-            ["PUID=" + str(puid), "PGID=" + str(pgid), "TZ=" + tz], 
-            ["{}/radarr:/config".format(config_path),
-            "{}/transmission/downloads:/torrents".format(config_path),
-            "{}/nzbget/downloads:/nzbs".format(config_path),
-            movie_path + ":/movies"],
-            [port])
-
-        compose_file["services"]["radarr"] = radarr_service
-
-        print("--------------------------")
-
-    if click.prompt("Jackett", default="y", type=str) == "y":
-        print("------JACKETT SETUP-------")
-        port = click.prompt("Port", default=9117, type=int)
-
-        jackett_service = create_service("linuxserver/jackett", 
-            "jackett", 
-            ["PUID=" + str(puid), "PGID=" + str(pgid), "TZ=" + tz], 
-            ["{}/jackett:/config".format(config_path)],
-            [port])
-
-        compose_file["services"]["jackett"] = jackett_service
-
-        print("--------------------------")
-
-    file = open("docker-compose.yml", "w")
-    file.write(yaml.dump(compose_file))
-    file.close()
-
-def create_service(image_name, container_name, env, volumes, ports):
-    """Creates a template for a docker compose service"""
-
-    return {
-        "image": image_name,
-        "container_name": container_name,
-        "environment": env,
-        "volumes": volumes,
-        "ports": ["{}:{}".format(p, p) for p in ports],
-        "restart": "always"
-    }
+questions = [
+    {
+        'type': 'input',
+        'name': 'puid',
+        'message': 'What is your PUID?',
+        'validate': lambda answer: answer.isdigit() or 'Your PUID should be an integer.',
+    },
+    {
+        'type': 'input',
+        'name': 'pgid',
+        'message': 'What is your PGID?',
+        'validate': lambda answer: answer.isdigit() or 'Your PGID should be an integer.',
+    },
+    {
+        'type': 'input',
+        'name': 'timezone',
+        'message': 'What timezone are you in?',
+        'default': 'America/New_York',
+    },
+    {
+        'type': 'input',
+        'name': 'config_path',
+        'message': 'What is the absolute path to the directory where all config files will be?',
+        'validate': lambda answer: os.path.exists(answer) or 'Please enter a valid directory path.',
+    },
+]
 
 if __name__ == "__main__":
-    generate()
+    answers = prompt(questions)
+    print_json(answers)
